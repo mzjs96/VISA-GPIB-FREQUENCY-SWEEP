3b4cd6a653e554e0b80d40d00ecc9b65a0d66278
Michael Z <mzjs96@gmail.com>
2019-02-20 12:04:11 -0800

    PYVISA
    PYVISA

--- pyvisa_mixer_freq_sweep.py
+++ pyvisa_mixer_freq_sweep.py
@@ -9,6 +9,7 @@ import collections
 class FreqSweep():
     def __init__(self, freq_start, freq_end, multiplier, version, sa_cent_freq):
         self.freq_start = freq_start
+        self.freq_step = 5
         self.freq_end = freq_end
         self.multiplier = multiplier
         self.version = version
@@ -56,6 +57,15 @@ class FreqSweep():
             sys.exit(1)
 
         print("Initialization Success. All instruments are connected!")
+        #set the output port to 2:
+
+        #Set the voltage protection for voltage source:
+        self.vs.write("VOLT:PROT:CLE")
+        self.vs.write("VOLT:PROT 0.5V")
+        self.vs.write("VOLT:PROT MAX")
+        self.vs.write("VOLT 0")
+        self.vs.write("OUTP ON")
+
         return 
 
     def biasing_calibration(self):
@@ -63,7 +73,7 @@ class FreqSweep():
         #Parameters for the voltage source sweep to be changed HERE:
 
         initial_voltage = 0.05 #Volts
-        volt_steps = 25  #steps to go up
+        volt_steps = 30  #steps to go up
         volt_step = 0.01
         
         #Sweeping through this range of voltage: 50 mV to 500 mV, step: 10mV for each frequency
@@ -79,15 +89,16 @@ class FreqSweep():
         self.sa.write('AVER:COUN 15')
 
         #Initialize the Signal Generator
-        cur_freq = self.freq_start
-        freq_step = 5
-        cur_sweep_freq = self.sweep_freq_start
+        curr_freq = self.freq_start
+        curr_sweep_freq = self.sweep_freq_start
 
         self.sg.write(':FREQ:FIX ' + str(self.sweep_freq_start) + ' GHz')
         self.sg.write(':FREQ:STEP ' + str(self.sweep_freq_step) + ' GHz')
         
         volt_pwr = collections.defaultdict()
-        
+
+        time.sleep(2)
+
         #iterate through the frequency range
         for _ in range(self.num_step):
             #iterate through all the voltages
@@ -102,6 +113,8 @@ class FreqSweep():
                 self.vs.write('VOLT ' + str(curr_volt + j * 0.01))
 
                 #measure the power and store accordingly
+                self.sa.write('CALC:MARK:CENT') 
+                time.sleep(1)
                 self.sa.write('CALC:MARK:Y?')
                 time.sleep(20)
                 meas_pwr = float(self.sa.read())
@@ -111,25 +124,30 @@ class FreqSweep():
                 curr_volt += volt_step
 
             #store highest voltage at current frequency into hashmap
-            self.freq_volt[cur_freq] = max(volt_pwr, key = volt_pwr.get)
+            self.freq_volt[curr_freq] = max(volt_pwr, key = volt_pwr.get)
+            print('Frequency: ' + curr_freq + ', Maximum power voltage: ' + self.freq_volt[curr_freq])
 
             #increment frequency
             self.sg.write(':FREQ UP')
-            cur_freq += freq_step
-            cur_sweep_freq += self.sweep_freq_step
+            time.sleep(1)
+            curr_freq += self.freq_step
+            curr_sweep_freq += self.sweep_freq_step
 
         self.is_calibrated = True
-        print('Success: Mapping of the voltage that produces highest power for each frequency (freq:volt):')
+        print('SUCCESS: Mapping of the voltage that produces highest power for each frequency (freq->volt)')
         print(self.freq_volt)
 
         #reset the voltage source and return
         self.vs.write('VOLT 0')
+        
+        #record map to local csv file
+        self.write_vmap_to_csv(self.freq_volt)
         return  
 
     def frequency_sweep(self):
         #Open the CSV file descriptor
         try:
-            with open('test_data.csv', 'wb') as csvfile:
+            with open('test_data.csv', 'a') as csvfile:
                 csvwriter = csv.writer(csvfile, delimiter = ' ', quotechar = '|')
         except:
             print('ERROR: CSV file open failed! Please check file path and name.')
@@ -152,10 +170,14 @@ class FreqSweep():
 
         #loading the biasing calibration:
         if not self.is_calibrated:
-            self.freq_volt = self.read_from_csv()
-
+            self.freq_volt = self.read_vmap_from_csv()
+        if len(self.freq_volt) == 0:
+            print("ERROR: Reading frequency voltage mapping csv file failed! Please check biasing calibration and try again!")
+            sys.exit(1)
+        
         curr_freq = self.freq_start
         curr_sweep_freq = self.sweep_freq_start
+        curr_volt = 0
 
         #Configure the Spectrum Analyzer:
         #Initialize the spectrum analyzer to frequency to be measured:
@@ -178,7 +200,14 @@ class FreqSweep():
             #edge case for frequency step when it reaches the range limit: (TO DO)
             #if i + 1 == self.num_step:
 
-            #calculate the value of center frequency
+            #Configure the voltage source to get the optimum voltage:
+            curr_volt = self.freq_volt[curr_freq]
+            if curr_volt > 0.5:
+                print("Error: Voltage is too high when sweeping frequency! Please check voltage source and try again.")
+                sys.exit(1)
+            self.vs.write('VOLT ' + curr_volt)
+
+            #set the marker at the center frequency
             self.sa.write('CALC:MARK:CENT')
 
             #calcualte the Y-coordinate of the center frequency (measure the peak power)
@@ -190,30 +219,42 @@ class FreqSweep():
             print("Current Frequency: " + curr_freq + 'Current Sweep Frequency: ' + curr_sweep_freq + ', Step: ' + i + ' , Measured Power: ' + meas_pwr)
 
             #increment the frequency by step
-            self.sg.write('FREQ UP')
+            self.sg.write(':FREQ UP')
+            curr_freq += self.freq_step
+            curr_sweep_freq += self.sweep_freq_step
+
             time.sleep(2)
 
             #write to CSV file:
             csvwriter.writerow(i + 1, curr_freq, curr_sweep_freq, meas_pwr)
+
+            #reset the voltage source to zero
+            self.vs.write('VOLT 0')
+            time.sleep(1)
         return
     
-    def write_to_csv(self, mydict):
+    def write_vmap_to_csv(self, mydict):
         with open('freq_volt_map.csv', 'w') as csv_file:
             writer = csv.writer(csv_file)
             for key, value in mydict.items():
                 writer.writerow([key, value])
     
-    def read_from_csv(self):
-        with open('dict.csv') as csv_file:
-            reader = csv.reader(csv_file)
-            mydict = dict(reader)
-        return mydict
+    def read_vmap_from_csv(self):
+        try:
+            with open('freq_volt_map.csv') as csv_file:
+                reader = csv.reader(csv_file)
+                mydict = dict(reader)
+            return mydict
+        except:
+            return None
 
 if __name__ == '__main__':
     #Parameters: start frequency, end frequency, mixer multiplier(1/3/18), version(0/1/2), spectrum analyzer center frequency (GHz)
     FS = FreqSweep(140, 200, 3, 0, 0.065)
     FS.initialize_instrument()
+    input("Initialization done. Press Enter to continue...")
     FS.biasing_calibration()
+    input("Calibration done. Press Enter to continue...")
     FS.frequency_sweep()
-
+    input("Frequency sweep done. Press Enter to exit...")
     sys.exit(0)
